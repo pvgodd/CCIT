deb32090d6ae497feaa15e12ab289fa40b156505
pvgodd <40848203+pvgodd@users.noreply.github.com>
2022-04-07 14:06:25 +0900

    Update test.bat

--- test.bat
+++ test.bat
@@ -7,7 +7,7 @@ echo %_OSARCH%
 
 
 
-if %_OSARCH% == x64 ( %SystemRoot%\SysWOW64\systeminfo.exe > ".\systeminfo.txt")
-if %_OSARCH% == x86 ( %SystemRoot%\Sysem32\systeminfo.exe > ".\systeminfo.txt")
+if %_OSARCH% == x64 ( %SystemRoot%\SysWOW64\systeminfo.exe > "..\systeminfo.txt")
+if %_OSARCH% == x86 ( %SystemRoot%\Sysem32\systeminfo.exe > "..\systeminfo.txt")
 
 ENDLOCAL
